848989ecf6f11e440adad296df38304eab7317ed
jairoy <jairo.yenecura@gmail.com>
2021-01-10 19:38:17 -0300

    Obtendo um post por id

---
+++ src/main/java/com/jairoy/workshopmongo/resource/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.jairoy.workshopmongo.resource.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.jairoy.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice //responsável para tratar possíveis erros de excessões
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;		
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não Encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);		
+	}
+
+}
--- src/main/java/com/jairoy/workshopmongo/services/UserService.java
+++ src/main/java/com/jairoy/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jairoy.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jairoy.workshopmongo.domain.User;
 import com.jairoy.workshopmongo.repositories.UserRepository;
+import com.jairoy.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();			
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);		
+		return user.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
