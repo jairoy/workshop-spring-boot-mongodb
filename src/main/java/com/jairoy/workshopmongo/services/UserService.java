5ce028cb43e951508a4e1604668c4c6ae346fc6b
jairoy <jairo.yenecura@gmail.com>
2021-01-10 17:34:53 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/jairoy/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.jairoy.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jairoy.workshopmongo.domain.User;
+import com.jairoy.workshopmongo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	
+	public List<User> findAll(){
+		return repo.findAll();			
+	}
+
+}
