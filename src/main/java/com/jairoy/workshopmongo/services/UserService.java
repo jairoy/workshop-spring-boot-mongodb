7af9b0d9b48a547976f8ea08216a8d53107df282
jairoy <jairo.yenecura@gmail.com>
2021-01-11 08:30:31 -0300

    Inserção de usuário com POST

--- src/main/java/com/jairoy/workshopmongo/services/UserService.java
+++ src/main/java/com/jairoy/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jairoy.workshopmongo.domain.User;
+import com.jairoy.workshopmongo.dto.UserDTO;
 import com.jairoy.workshopmongo.repositories.UserRepository;
 import com.jairoy.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);		
 		return user.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
